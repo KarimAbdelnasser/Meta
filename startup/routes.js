5f405958d151a4c786961e920405274c64a203db
KarimAbdelnasser <kareemabdo38@yahoo.com>
2022-10-24 03:53:02 +0200

    v1.2

--- startup/routes.js
+++ startup/routes.js
@@ -5,12 +5,14 @@ const addFriend = require("../routes/addFriend");
 const notifications = require("../routes/notifications");
 const timeline = require("../routes/timelines");
 
+//All Routes
+
 module.exports = (app) => {
-  app.use(express.json());
-  app.use(helmet());
-  app.use(express.static("public"));
-  app.use("/users", users);
-  app.use("/users", addFriend);
-  app.use("/users", notifications);
-  app.use("/users", timeline);
+    app.use(express.json());
+    app.use(helmet());
+    app.use(express.static("public"));
+    app.use("/users", users);
+    app.use("/users", addFriend);
+    app.use("/users", notifications);
+    app.use("/users", timeline);
 };
